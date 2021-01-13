a6d3bd73d8dfc48d972e9e2a2627e8b7a72b7478
lukevangorder <lukevangorder@gmail.com>
2021-01-13 15:53:39 -0500

    commit tree 6e066b3baa939f86faed012d11f138bc22197dfc

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+    i=0
+    sendBack = []
+    while i<collection.length
+        sendBack << yield(collection[i])
+        i+=1
+    end
+    if sendBack.include?(false)
+        false
+    else
+        true
+    end
 end
